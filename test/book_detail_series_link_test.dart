c0881ff6818a00ca7f08dde63d758f0e05e1a4b2
Kanasara <81666000+Roroca-1@users.noreply.github.com>
2026-09-05 12:50:26 -0400

    test: remove unused merged model import

--- test/book_detail_series_link_test.dart
+++ test/book_detail_series_link_test.dart
@@ -9,7 +9,6 @@ import 'package:lightnovel_shelf_plus/core/network/request_scheduler.dart';
 import 'package:lightnovel_shelf_plus/core/platform/stores.dart';
 import 'package:lightnovel_shelf_plus/core/network/signalr_connection.dart';
 import 'package:lightnovel_shelf_plus/data/api/api_client.dart';
-import 'package:lightnovel_shelf_plus/data/api/models.dart';
 import 'package:lightnovel_shelf_plus/data/providers.dart';
 import 'package:lightnovel_shelf_plus/data/repositories/local_comic_shelf_repository.dart';
 import 'package:lightnovel_shelf_plus/data/retry_policy.dart';
